665f4b49eed5ebb55ca763b4568f7e3130f966de
Irina.Constantinova <irina.constantinova@gmail.com>
2024-01-25 21:25:06 +0300

    added copy_data.py

---
+++ copy_data.py
@@ -0,0 +1,14 @@
+from return_data_file import data_file
+
+def copy_row():
+    data, nf = data_file()
+    count_rows = len(data)
+    if count_rows == 0:
+        print("Файл пусто!")
+    else:
+        number_row = int(input(f"Введите номер строки "
+                               f"от 1 до {count_rows}: "))
+        while number_row < 1 or number_row > count_rows:
+            number_row = int(input(f"Ошибка!"
+                                   f"Введите номер строки "
+                                   f"от 1 до {count_rows}: "))
