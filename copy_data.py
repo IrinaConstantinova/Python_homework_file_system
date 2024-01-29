f8e676e697b71a58b2cc9fbd5916e4f94367a2a0
Irina.Constantinova <irina.constantinova@gmail.com>
2024-01-29 18:42:04 +0300

    added changes in file copy_data

--- copy_data.py
+++ copy_data.py
@@ -1,31 +1,26 @@
 from return_data_file import data_file
 
 def copy_row():
-    data, nf = data_file()
-    count_rows = len(data)
+    data1, nf1 = data_file()
+    count_rows = len(data1)
     if count_rows == 0:
         print("Файл пусто!")
     else:
-        number_row = int(input(f"Введите номер строки "
+        number_row1 = int(input(f"Введите номер строки "
                                f"от 1 до {count_rows}: "))
-        while number_row < 1 or number_row > count_rows:
-            number_row = int(input(f"Ошибка!"
+        while number_row1 < 1 or number_row1 > count_rows:
+            number_row1 = int(input(f"Ошибка!"
                                    f"Введите номер строки "
                                    f"от 1 до {count_rows}: "))
-        
-        with open(f'db/data_{nf}.txt', 'r', encoding='utf-8') as file1:
-            lines_file1 = file1.readlines()
-            line_to_copy = data[number_row - 1]
-            file1.readline(line_to_copy)
-        data, nf = data_file()
-        with open(f'db/data_{nf}.txt', 'r', encoding='utf-8') as file2:
-            lines_file2 = file2.readlines()
-            last_number = int(lines_file2[-1].split(';')[0]) if lines_file2 else 0
-        with open(f'db/data_{nf}.txt', 'a', encoding='utf-8') as file2:
-            for line in lines_file1:
-                last_number += 1
-                new_line = f'{last_number} {line.split(maxsplit=1)[1]}'
-                file2.write(new_line)
-
-        
+        with open(f'db/data_{nf1}.txt', 'r', encoding='utf-8') as file1:
+            lines1 = file1.readlines()
+            line_to_copy = lines1[number_row1 - 1] 
+        data2, nf2 = data_file()
+        # count_rows2 = len(data2)
+        last_number2 = int(data2[-1].split(';')[0]) if data2 else 0
+        new_number2 = last_number2 + 1
+        line_to_copy = f'{new_number2};{line_to_copy.split(';')[1]};{line_to_copy.split(';')[2]};{line_to_copy.split(';')[3]};{line_to_copy.split(';')[4]}'
+        data2.append(line_to_copy)
+        with open(f'db/data_{nf2}.txt', 'w', encoding='utf-8') as file2:
+            file2.writelines(data2)
         print("Строка успешно скопирована!")
